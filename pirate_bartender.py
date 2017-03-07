234fbde03f1df1cdfd4d14299a5a4ac5c1f636ef
Ryan Kuhle <ryan@ryankuhle.com>
2017-03-07 22:53:16 +0000

    Layout drink_preferences function

---
+++ pirate_bartender.py
@@ -0,0 +1,25 @@
+def drink_preferences():
+    """ drink_preferences
+    Asks customer what their drink preferences are based on dictionary of
+    questions. Customer will answer as 'y' or 'n'. Answers will be stored in
+    new dictionary as Boolean value. Function returns dictionary of answers.
+    """
+    questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+    }
+    
+    # Create loop that asks questions.
+    # Error check for 'y' or 'n'.
+    # Save answers in 'answers' dictionary as Booleans.
+    ## Example: 'strong': True
+    
+    return answers
+
+
+
+
+a = drink_preferences()
